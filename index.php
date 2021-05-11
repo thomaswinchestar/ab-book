8376f199644416f26062b15f757097e06f9b203d
Devpph <52081212+thomaswinchestar@users.noreply.github.com>
2021-05-12 05:30:20 +0630

    table width set to 50%

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <style>
         .table{
-            width: 90%;
+            width: 50%;
             margin: 100px auto;
         }
     </style>
